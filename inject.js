26e616743c50d408f501ae47b6f53eb127e721ba
bzle <bzelll@gmail.com>
2016-04-07 14:51:34 -0400

    Changed icon and added !important to css

--- inject.js
+++ inject.js
@@ -4,7 +4,7 @@
         var blocking_css = document.createElement("div");
         blocking_css.id = "blocking_css";
         document.body.appendChild(blocking_css);
-        document.getElementById("blocking_css").innerHTML = "<style>body a[target=\"_blank\"]{border: 0px solid transparent;}</style>";
+        document.getElementById("blocking_css").innerHTML = "<style>body a[target=\"_blank\"]{border: 0px solid transparent !important;}</style>";
     } else {
         var elem = document.getElementById("blocking_css");
         elem.remove();
